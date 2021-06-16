0c24008ad784377c2411e0b92e79a25a421a3b15
Sarah Rainsberger <5098874+sarah11918@users.noreply.github.com>
2021-06-16 12:42:11 -0300

    Initial commit

---
+++ src/pages/join.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Join = () => {
+  return (
+    <Layout>
+      <SEO title="Join the Choir" />
+      <h1>Join Summerside Community Choir!</h1>
+      <p>Here is some info about joining the choir.</p>
+    </Layout>
+  )
+}
+
+export default Join
