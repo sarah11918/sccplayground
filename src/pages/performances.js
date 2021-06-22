c8f64bddc2381a958b6c8ec3bf2def48f5bed3d9
Sarah Rainsberger <5098874+sarah11918@users.noreply.github.com>
2021-06-22 10:16:35 -0300

    Prepared performance page for adding images and links to display upcoming and past performances

--- src/pages/performances.js
+++ src/pages/performances.js
@@ -1,4 +1,6 @@
 import React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
@@ -6,8 +8,9 @@ const Performances = () => {
   return (
     <Layout>
       <SEO title="Summerside Choir Performances" />
-      <h1>Performances</h1>
+      <h1>Upcoming Performances</h1>
       <p>Here is some info about performances.</p>
+      <h2>Past Performances</h2>
     </Layout>
   )
 }
