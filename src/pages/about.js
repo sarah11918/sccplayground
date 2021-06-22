391794d943b0994e98568b667ee69a563ca55a86
Sarah Rainsberger <5098874+sarah11918@users.noreply.github.com>
2021-06-21 21:17:32 -0300

    Added text and image to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Layout from "../components/layout"
+import {StaticImage} from "gatsby-plugin-image"
 import SEO from "../components/seo"
 
 const About = () => {
@@ -7,7 +8,9 @@ const About = () => {
     <Layout>
       <SEO title="About" />
       <h1>About The Choir</h1>
-      <p>Here will be some more info about the choir.</p>
+      <p>The Summerside Community Choir is an open membership, mixed-voice community choir who welcomes new members at the beginning of each term to work towards an end-of-term concert. We perform two Christmas concerts in December and two spring concerts in May or June and may have extra events throughout the term.</p>
+      <p>In 1978, Bill Leuty had a dream to start a community choir in Summerside, PEI, Canada.  He was able to make this dream a reality, and in 2018 the 50-plus member Summerside Community Choir celebrated forty years of choral music.</p>
+      <StaticImage src="../images/35thAnniversaryCake.jpg" alt="35th Anniversary Cake" />
     </Layout>
   )
 }
